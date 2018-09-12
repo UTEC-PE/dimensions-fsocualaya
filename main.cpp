2bd84d07c2ec57ce26be48d82546e4d4b21a2e5a
fsocualaya <fernando.socualaya@utec.edu.pe>
2018-09-11 22:09:37 -0500

    Added operator

--- main.cpp
+++ main.cpp
@@ -6,9 +6,18 @@
 using namespace std;
 
 template <typename T>
-struct Dimensions {      
+struct Dimensions {
+      // sizes : tamaño de cada diemnsion
+      // coordinates: coordenadas
+      // dimensions: cantidad de dimensiones
     int operator()(int* coordinates, int* sizes, int dimensions) {
         // TODO
+        int position = 0;
+        for(int i=dimensions-1;i>=0;--i){
+            position *= sizes[i];
+            position += coordinates[i];
+        }
+        return position;
     }
 };
 
@@ -18,6 +27,9 @@ struct Integer {
 };
 
 int main(int argc, char *argv[]) {
+    int dimsz[3] = {2,2,2};
+    Vector<Integer> v(3, dimsz);
+    // cout<<v({1,0,1},dimsz,3);
     system("pause");
     return EXIT_SUCCESS;
 }
--- vector.h
+++ vector.h
@@ -6,24 +6,33 @@ class Vector {
     public:
         typedef typename Tr::T T;
         typedef typename Tr::Operation Operation;
-             
+
     private:
-        T* data;
+        T* data; //cantidad de elementos width*height
         int dataSize;
 
         int dimensions;
         int* dimensionSizes;
 
+        Operation op;
+
     public:
         Vector() : data(nullptr) {};
-             
+
+        //dimensions: cantidad de dimensiones
+        //dimensionSizes: tamaño de cada dimension
         Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
             // TODO
-        }
-             
+            dataSize = 1;
+            for(int i=0;i<dimensions;i++)
+                dataSize *= dimensionSizes[i];
+            data = new T[dataSize];
+        };
+
         void set(T datum, int* coordinates); // TODO
-             
+
         T get(int* coordinates); // TODO
+        ~Vector(){};
 };
 
 #endif
